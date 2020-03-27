9efa83b525396d7115e0b599550833b4918e52da
Kr328 <39107975+Kr328@users.noreply.github.com>
2020-03-27 12:11:26 +0800

    binding: remove unused lock

--- binding/pool.go
+++ binding/pool.go
@@ -14,9 +14,6 @@ func NewPortPool() *PortPool {
 }
 
 func (pool *PortPool) Next() uint16 {
-	pool.lock.Lock()
-	defer pool.lock.Unlock()
-
 	pool.current = uint16(uint32(pool.current+1) % 65536)
 	return pool.current
 }
